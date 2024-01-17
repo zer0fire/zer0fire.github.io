5f4502eaede6d78a25314496bf2f4837ac1df9b2
wangmeng <wangmeng08@sparkedu.com>
2024-01-17 11:13:23 +0800

    Site updated: YYYY-01-Jan 17, 2024 11:13:23

--- js/circle.js
+++ js/circle.js
@@ -1,11 +1,1 @@
-const circle = document.createElement("div");
-document.body.appendChild(circle);
-const header = document.querySelector("header");
-circle.classList = ["circle"];
-document.body.insertBefore(circle, header);
-const rect = circle.getBoundingClientRect();
-
-window.addEventListener("mousemove", (e) => {
-  circle.style.top = `${e.clientY - rect.height / 2}px`;
-  circle.style.left = `${e.clientX - rect.width / 2}px`;
-});
+const circle=document.createElement("div"),header=(document.body.appendChild(circle),document.querySelector("header")),rect=(circle.classList=["circle"],document.body.insertBefore(circle,header),circle.getBoundingClientRect());window.addEventListener("mousemove",e=>{circle.style.top=e.clientY-rect.height/2+"px",circle.style.left=e.clientX-rect.width/2+"px"});
