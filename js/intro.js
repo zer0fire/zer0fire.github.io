59275add1af4abd7be90aa9bd4cacc1583e29d3f
wangmeng <wangmeng08@sparkedu.com>
2024-03-10 21:15:15 +0800

    Site updated: YYYY-03-Mar 10, 2024 21:15:15

--- js/intro.js
+++ js/intro.js
@@ -1,7 +1,17 @@
-if("/"===window.location.pathname){const a=document.createElement("div"),b=(a.classList=["intro"],a.innerHTML=`
+if (window.location.pathname === "/") {
+  const intro = document.createElement("div");
+  intro.classList = ["intro"];
+
+  intro.innerHTML = `
     <h1>Alex Wang</h1>
     <h3>A Software Developer who always wants to be better in tech and life.</h3>
     
     <a href="/about">About Me</a>
     <a href="https://github.com/zer0fire" target="_blank">Github</a>
-    `,document.querySelector(".banner-text.text-center.fade-in-up .h2"));b.appendChild(a)}
+    `;
+
+  const container = document.querySelector(
+    ".banner-text.text-center.fade-in-up .h2"
+  );
+  container.appendChild(intro);
+}
